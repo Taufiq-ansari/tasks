2f27f594289eb44e70dd1e37be904d895e504c85
Bhumika Patel <bhumika.patel@macymind.in>
2026-02-05 17:09:03 +0530

    fetchapi done

--- lib/users_apis/api.dart
+++ lib/users_apis/api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,44 +13,99 @@ class usersScreen extends StatefulWidget {
 
 class _usersScreenState extends State<usersScreen> {
   List user = [];
+  bool isloading = false;
   @override
   void initState() {
     super.initState();
     fetchData();
   }
 
+  //  send  data  to the  internet
+
+  postData() {}
+
+//  fetch data fron internet
   fetchData() async {
+    setState(() {
+      isloading = true;
+    });
+
+    await Future.delayed(Duration(seconds: 2));
     final String url = "https://dummyjson.com/users#";
     final Uri uri = Uri.parse(url);
-    final response = await http.get(uri);
-    final decodedData = jsonDecode(response.body);
-    user = decodedData["users"];
-    print(user);
-    setState(() {});
+    final response = await http.get(
+      uri,
+    );
+    if (response.statusCode == 200) {
+      final decodedData = jsonDecode(response.body);
+      user = decodedData["users"];
+      print("${response.statusCode}");
+      print(user);
+    } else {
+      print(response.statusCode);
+    }
+
+    // if you wanna find specific users address then use like this given below
+    // print(user[3]["address"]["address"]);
+    setState(() {
+      isloading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "User Api",
-            style: TextStyle(
-                // fontFamily: "Myfonts",
+    return CupertinoPageScaffold(
+      // appBar: AppBar(
+      //   title: Text(
+      //     "User Api",
+      //     style: TextStyle(
+      //         // fontFamily: "Myfonts",
 
-                ),
-          ),
-          actions: [
-            Icon(
-              Icons.more_vert_outlined,
+      //         ),
+      //   ),
+      //   actions: [
+      //     Icon(
+      //       Icons.more_vert_outlined,
+      //     ),
+      //   ],
+      // ),
+      navigationBar: CupertinoNavigationBar(
+        leading: Text("Users Api"),
+
+        // trailing: ,
+      ),
+      child: isloading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemCount: user.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  // color: Colors.white70,
+                  child: CupertinoListTile(
+                    leading: Image.network("${user[index]["image"]}"),
+                    title: Text(
+                      "${user[index]["firstName"]} ${user[index]["lastName"]}",
+                      style: TextStyle(
+                        fontFamily: "tangerine",
+                        fontSize: 20,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "${user[index]["email"]}",
+                      style: TextStyle(
+                        // fontFamily: "Myfonts",
+                        fontSize: 12,
+                      ),
+                    ),
+                    trailing: Text(
+                      "age:${user[index]["age"]}",
+                      style: TextStyle(fontFamily: "tangerine", fontSize: 20),
+                    ),
+                  ),
+                );
+              },
             ),
-          ],
-        ),
-        body: ListView.builder(
-          itemCount: user.length,
-          itemBuilder: (context, snapshot) {
-            return Text("${user[snapshot]["lastName"]}");
-          },
-        ));
+    );
   }
 }
